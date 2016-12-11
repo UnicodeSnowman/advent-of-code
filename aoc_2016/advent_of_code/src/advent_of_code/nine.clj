f1557db22e0d44f78d89cb54c922b2bbfa54de73
unicodesnowman <utfsnowman@gmail.com>
2016-12-10 19:33:46 -0500

    hideous version of 9, but working)

---
+++ aoc_2016/advent_of_code/src/advent_of_code/nine.clj
@@ -0,0 +1,34 @@
+(ns advent-of-code.nine)
+
+(def compressed-data (slurp
+                       (str
+                          (-> (ClassLoader/getSystemResource *file*) clojure.java.io/file .getParent)
+                          "/nine.txt")))
+
+(defn get-marker [data]
+  (let [match (re-find #"^\(([0-9]+)x([0-9]+)\)" data)]
+    (if match
+      [(nth match 1) (nth match 2)])))
+
+(defn compressed-length [compressed-data]
+  ((fn [cnt data]
+     (if (seq data)
+       (let [[num-chars times] (get-marker data)]
+              (if (and num-chars times)
+                (recur (+ cnt (* (Integer. num-chars) (Integer. times)))
+                       (subs data (+
+                                (count (str "(" num-chars "x" times ")"))
+                                (Integer. num-chars))))
+                (recur (inc cnt)
+                       (subs data 1))))
+       cnt))
+   0 compressed-data))
+
+; ADVENT   : 6
+; A(1x5)BC : 7
+; (3x3)XYZ : 9
+; A(2x2)BCD(2x2)EFG : 11
+; (6x1)(1x3)A : 6
+; X(8x2)(3x3)ABCY : 18
+
+(compressed-length (clojure.string/trim-newline compressed-data))
