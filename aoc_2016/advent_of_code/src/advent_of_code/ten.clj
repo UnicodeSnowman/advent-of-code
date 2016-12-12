56681d9ee452646c721c1cd09a99716c3fc6dbfa
unicodesnowman <utfsnowman@gmail.com>
2016-12-12 18:20:53 -0500

    ideas, updates

--- aoc_2016/advent_of_code/src/advent_of_code/ten.clj
+++ aoc_2016/advent_of_code/src/advent_of_code/ten.clj
@@ -233,22 +233,24 @@
   "bot 53 gives low to output 0 and high to bot 6"
   "bot 48 gives low to bot 190 and high to bot 156"])
 
-
-
 ; can define values based on [1 :low] (i.e. [bot# low-or-high]), then
 ; recursively trace them back (perhaps at the end? or does it need to be
 ; done/cleaned up after each step?) to determine values
 
 (defmethod process-instruction :bot [acc instr]
   ; TODO handle giving to output too...
-  (let [[_ giver-bot receive-low-bot receive-high-bot] (re-find #"bot ([0-9]+) gives low to [bot|output] ([0-9]+) and high to [bot|output] ([0-9]+)" instr)]
-    (prn giver-bot receive-low-bot receive-high-bot)))
+  (let [[_ giver-bot low-ident receive-low-id high-ident receive-high-id] (re-find #"bot ([0-9]+) gives low to (bot|output) ([0-9]+) and high to (bot|output) ([0-9]+)" instr)]
+    acc))
 
 (defmethod process-instruction :value [acc instr]
-  (let [[_ value receive-bot] (re-find #"value ([0-9]+) goes to bot ([0-9]+)" instr)]
-    (prn value receive-bot)))
+  (let [[_ value receive-bot] (re-find #"value ([0-9]+) goes to bot ([0-9]+)" instr)
+        path [:bots receive-bot :values]]
+    (assoc-in
+      acc
+      path
+      (conj (get-in acc path []) value))))
 
-(reduce process-instruction {} instructions)
+(reduce process-instruction { :bots {} :output {} } instructions)
 
 {
   2 { :values [2 5] :low 2 :high 5 }
